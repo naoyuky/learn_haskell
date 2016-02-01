b8b67e2000793a5b7aa3f71b2369a5fc935e536f
Naoyuki UEDA <naoyuki8585@gmail.com>
2016-02-01 17:48:43 +0900

    edit ex4.hs:removed do

--- exercise-4-brain-twister/untitled.hs
+++ exercise-4-brain-twister/untitled.hs
@@ -5,7 +5,7 @@ wordreverse xs = do
     take 1 xs ++ (reverse $ drop 1 (take ((length xs) - 1) xs )) ++ drop (length xs -1) xs
 
 main :: IO ()
-main = do
+main = 
     strings <- getLine
     putStrLn $unwords $map wordreverse $words strings
     f<- isEOF
